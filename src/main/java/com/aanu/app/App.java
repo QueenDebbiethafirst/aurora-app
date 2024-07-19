a8bb2c1b20ce03e6aa327bc73dd3532c689739cb
QueenDebbiethafirst <anuobisesan@gmail.com>
2024-07-19 11:53:44 +0100

    updated for web server

--- src/main/java/com/aanu/app/App.java
+++ src/main/java/com/aanu/app/App.java
@@ -1,13 +1,24 @@
 package com.aanu.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!, This is Aanu, My first Java Script. How is learning going?" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+        System.out.println("Hello World!, This is Aanu, My first Java Script. How is learning going?");
+    }
+}
+
+@RestController
+@RequestMapping("/hello")
+class HelloController {
+    @GetMapping
+    public String sayHello() {
+        return "Hello World!, This is Aanu, My first Java Script. Operating on my Console and web server, How is learning going on your end?";
     }
 }
